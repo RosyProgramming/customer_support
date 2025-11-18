9b5862f9aa60bd94d7ebb056f58e17b1e2458066
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-11-17 23:08:29 -0300

    Estilizando Loginbox do Chatbot

--- chat/src/components/LoginBox/LoginBox.jsx
+++ chat/src/components/LoginBox/LoginBox.jsx
@@ -1,13 +1,25 @@
+import { useRef } from "react";
 import "./LoginBox.css";
 
 export default function Loginbox({ setEmail }){
+    const emailInput = useRef(null);
+
     function login(){
-        setEmail("beatriz.costa@example.com");
+        const email = emailInput.current.value;
+        
+        if(email.length === 0){
+            return;
+        }
+
+        setEmail(email);
+        // setEmail("beatriz.costa@example.com");
     }
     return <>
-    <div>
-        <input></input>
-        <button onClick={login}>Login</button>
+    <div class="login-background">
+        <div className="login-div">
+            <input className="login-input" ref={emailInput}></input>
+            <button className="login-button" onClick={login}>Login</button>
+        </div>
     </div>
     </>
 }
